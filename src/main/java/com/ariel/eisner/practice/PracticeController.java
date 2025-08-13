688fe54049a8ea8032a89f6a07de6d039d5c3734
Ariel Eisner <ariel.eisner@odigeo.org>
2025-08-13 12:55:19 +0200

    Grouped anagrams.

--- src/main/java/com/ariel/eisner/practice/PracticeController.java
+++ src/main/java/com/ariel/eisner/practice/PracticeController.java
@@ -1,10 +1,14 @@
 package com.ariel.eisner.practice;
 
+import com.ariel.eisner.practice.dto.GroupAnagramsRequest;
 import com.ariel.eisner.practice.dto.TwoSumRequest;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("/api")
@@ -35,16 +39,16 @@ public class PracticeController {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j}; // Return the sum of indices
+                    return new int[]{i, j};
                 }
             }
         }
-        return new int[0]; // Return an empty array if no solution is found
+        return new int[0];
     }
 
     /**
      * LeetCode Problem 3: Longest Substring Without Repeating Characters
-     * https://leetcode.com/problems/longest-substring-without-repeating-characters/
+     * <a href="https://leetcode.com/problems/longest-substring-without-repeating-characters/">...</a>
      * Given a string s, find the length of the longest substring without duplicate characters.
      * Example 1:
      * Input: s = "abcabcbb"
@@ -74,5 +78,42 @@ public class PracticeController {
             }
         }
         return maxLength;
+    }
+
+    /**
+     * Anagrams Grouping
+     * Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+     * https://leetcode.com/problems/group-anagrams/
+     *
+     * Example 1:
+     * Input: strs = ["eat","tea","tan","ate","nat","bat"]
+     * Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+     * Explanation:
+     * There is no string in strs that can be rearranged to form "bat".
+     * The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
+     * The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.
+     *
+     * Example 2:
+     * Input: strs = [""]
+     * Output: [[""]]
+     *
+     * Example 3:
+     * Input: strs = ["a"]
+     * Output: [["a"]]
+     *
+     * Constraints:
+     * 1 <= strs.length <= 104
+     * 0 <= strs[i].length <= 100
+     * strs[i] consists of lowercase English letters.
+     */
+    @PostMapping("/groupAnagrams")
+    public List<List<String>> groupAnagrams(@RequestBody GroupAnagramsRequest request) {
+        Map<String, List<String>> collect = request.words().stream()
+                .collect(Collectors.groupingBy(word -> word.chars()
+                        .mapToObj(c -> (char) c)
+                        .sorted((c1, c2) -> c2 - c1)
+                        .map(String::valueOf)
+                        .collect(Collectors.joining())));
+        return new ArrayList<>(collect.values());
     }
 }
---
+++ src/main/java/com/ariel/eisner/practice/dto/GroupAnagramsRequest.java
@@ -0,0 +1,7 @@
+package com.ariel.eisner.practice.dto;
+
+import java.util.List;
+
+public record GroupAnagramsRequest(
+   List<String> words
+) {}

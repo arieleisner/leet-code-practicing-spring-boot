a1acd9154a0e25abbc6faad3cc4dffcf6c1134db
Ariel Eisner <ariel.eisner@odigeo.org>
2025-08-21 13:56:04 +0200

    Add tests for anagram (no tdd here :sad:)

--- src/test/java/com/ariel/eisner/practice/PracticeControllerTest.java
+++ src/test/java/com/ariel/eisner/practice/PracticeControllerTest.java
@@ -1,6 +1,11 @@
 package com.ariel.eisner.practice;
 
+import com.ariel.eisner.practice.dto.GroupAnagramsRequest;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.assertj.core.api.Assertions.*;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -29,5 +34,23 @@ class PracticeControllerTest {
         assertEquals(2, practiceController.longestSubstringWithoutRepeatingCharacters("cdcdcdcd"));
         assertEquals(3, practiceController.longestSubstringWithoutRepeatingCharacters("cdcdecdcd"));
         assertEquals(4, practiceController.longestSubstringWithoutRepeatingCharacters("cdcdecdcdef"));
+    }
+
+
+    @Test
+    public void testGroupAnagrams() {
+        PracticeController practiceController = new PracticeController();
+        equalsIgnoringOrder(List.of(), practiceController.groupAnagrams(new GroupAnagramsRequest(List.of())));
+        equalsIgnoringOrder(List.of(List.of("a")), practiceController.groupAnagrams(new GroupAnagramsRequest(List.of("a"))));
+        equalsIgnoringOrder(List.of(List.of("a"), List.of("b")), practiceController.groupAnagrams(new GroupAnagramsRequest(List.of("a", "b"))));
+        equalsIgnoringOrder(List.of(List.of("ab", "ba"), List.of("b")), practiceController.groupAnagrams(new GroupAnagramsRequest(List.of("ab", "b", "ba"))));
+        equalsIgnoringOrder(List.of(List.of("ate", "eat", "tea"), List.of("nat", "tan"), List.of("bat")), practiceController.groupAnagrams(new GroupAnagramsRequest(List.of("eat", "tea", "tan", "ate", "nat", "bat"))));
+    }
+
+    private void equalsIgnoringOrder(List<List<String>> expected, List<List<String>> actual) {
+        Assertions.assertThat(actual)
+                .usingRecursiveComparison()
+                .ignoringCollectionOrder()
+                .isEqualTo(expected);
     }
 }

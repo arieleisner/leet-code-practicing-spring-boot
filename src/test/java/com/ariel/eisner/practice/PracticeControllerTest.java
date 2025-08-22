066fb9122887fa783c0762536fa5394a7413cafa
Ariel Eisner <ariel.eisner@odigeo.org>
2025-08-22 16:16:46 +0200

    Add tests for two sum

--- src/test/java/com/ariel/eisner/practice/PracticeControllerTest.java
+++ src/test/java/com/ariel/eisner/practice/PracticeControllerTest.java
@@ -1,6 +1,7 @@
 package com.ariel.eisner.practice;
 
 import com.ariel.eisner.practice.dto.GroupAnagramsRequest;
+import com.ariel.eisner.practice.dto.TwoSumRequest;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.assertj.core.api.Assertions.*;
@@ -26,7 +27,30 @@ class PracticeControllerTest {
     }
 
     @Test
-    void twoSum() {
+    void twoSumWithoutFindResult() {
+        PracticeController practiceController = new PracticeController();
+        int[] nums = new int[]{1, 2, 3, 4, 5};
+        int[] expected = new int[0];
+        TwoSumRequest twoSumRequest = new TwoSumRequest(nums, 2);
+
+        int[] actual = practiceController.twoSum(twoSumRequest);
+
+        assertEquals(expected.length, actual.length);
+        assertEquals(0, actual.length);
+    }
+
+    @Test
+    void twoSumWithPositiveResult() {
+        PracticeController practiceController = new PracticeController();
+        int[] nums = new int[]{1, 2, 3, 4, 5};
+        int[] expected = new int[]{0, 1};
+        TwoSumRequest twoSumRequest = new TwoSumRequest(nums, 3);
+
+        int[] actual = practiceController.twoSum(twoSumRequest);
+
+        assertEquals(expected.length, actual.length);
+        assertEquals(expected[0], actual[0]);
+        assertEquals(expected[1], actual[1]);
     }
 
     @Test

6ce91ddaad48f42a0105b1a66be96018a00c239f
Ariel Eisner <aeeisnerg@gmail.com>
2026-02-18 15:12:08 +0100

    Removing duplicate code in test.

--- src/test/java/com/ariel/eisner/practice/application/usecase/TwoDimensionIteratorServiceTest.java
+++ src/test/java/com/ariel/eisner/practice/application/usecase/TwoDimensionIteratorServiceTest.java
@@ -1,30 +1,31 @@
 package com.ariel.eisner.practice.application.usecase;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TwoDimensionIteratorServiceTest {
 
+    private TwoDimensionIteratorService<Integer> service;
+
+    @BeforeEach
+    void setUp() {
+        service = new TwoDimensionIteratorService<>();
+    }
+
     @Test
     public void objectCreationTest() {
-        TwoDimensionIteratorService<Integer> service = new TwoDimensionIteratorService<>();
-
         assertNotNull(service);
     }
 
     @Test
     public void nextIsNullTest() {
-        TwoDimensionIteratorService<Integer> service = new TwoDimensionIteratorService<>();
-
         assertNull(service.next());
     }
 
     @Test
     public void hasNextIsFalseTest() {
-        TwoDimensionIteratorService<Integer> service = new TwoDimensionIteratorService<>();
-
         assertFalse(service.hasNext());
     }
-
 }

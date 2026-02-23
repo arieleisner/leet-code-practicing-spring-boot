38b2132dc07afe2046c440d252ce1978aeacf7ea
Ariel Eisner <aeeisnerg@gmail.com>
2026-02-23 18:38:34 +0100

    Add case for multiple rows and empty values.

--- src/main/java/com/ariel/eisner/practice/application/usecase/TwoDimensionIteratorService.java
+++ src/main/java/com/ariel/eisner/practice/application/usecase/TwoDimensionIteratorService.java
@@ -3,13 +3,54 @@ package com.ariel.eisner.practice.application.usecase;
 import com.ariel.eisner.practice.domain.port.in.TwoDimensionIteratorUseCase;
 
 public class TwoDimensionIteratorService<T> implements TwoDimensionIteratorUseCase<T> {
+
+    private final T[][] data;
+    private int row;
+    private int col;
+
+    public TwoDimensionIteratorService(T[][] data) {
+        this.data = data;
+        row = 0;
+        col = 0;
+        advanceToNextValid();
+    }
+
+    private void advanceToNextValid() {
+        boolean hasNextValid = false;
+        int firstCol = col;
+        for (int i = row; i < data.length; ++i) {
+            for (int j = firstCol; j < data[i].length; ++j) {
+                if (data[i][j] != null) {
+                    hasNextValid = true;
+                    row = i;
+                    col = j;
+                    break;
+                }
+            }
+            if (hasNextValid) {
+                break;
+            } else {
+                firstCol = 0;
+            }
+        }
+        if (!hasNextValid) {
+            row = data.length;
+        }
+    }
+
     @Override
     public T next() {
-        return null;
+        T toReturn = null;
+        if (hasNext()) {
+            toReturn = data[row][col];
+            ++col;
+            advanceToNextValid();
+        }
+        return toReturn;
     }
 
     @Override
     public Boolean hasNext() {
-        return Boolean.FALSE;
+        return row < data.length;
     }
 }
--- src/test/java/com/ariel/eisner/practice/application/usecase/TwoDimensionIteratorServiceTest.java
+++ src/test/java/com/ariel/eisner/practice/application/usecase/TwoDimensionIteratorServiceTest.java
@@ -1,6 +1,5 @@
 package com.ariel.eisner.practice.application.usecase;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,24 +7,90 @@ import static org.junit.jupiter.api.Assertions.*;
 class TwoDimensionIteratorServiceTest {
 
     private TwoDimensionIteratorService<Integer> service;
-
-    @BeforeEach
-    void setUp() {
-        service = new TwoDimensionIteratorService<>();
-    }
+    private Integer[][] matrix;
 
     @Test
     public void objectCreationTest() {
+        matrix = new Integer[0][];
+        service = new TwoDimensionIteratorService<>(matrix);
+
         assertNotNull(service);
     }
 
     @Test
     public void nextIsNullTest() {
+        matrix = new Integer[0][];
+        service = new TwoDimensionIteratorService<>(matrix);
+
         assertNull(service.next());
     }
 
     @Test
-    public void hasNextIsFalseTest() {
+    public void hasNextIsFalseWhenNoElementsTest() {
+        matrix = new Integer[0][];
+        service = new TwoDimensionIteratorService<>(matrix);
+
         assertFalse(service.hasNext());
+    }
+
+    @Test
+    public void hasNextIsTrueWithOneElementTest() {
+        matrix = new Integer[][] { { 0 } };
+        service = new TwoDimensionIteratorService<>(matrix);
+
+        assertTrue(service.hasNext());
+        assertEquals(0, service.next());
+        assertFalse(service.hasNext());
+        assertNull(service.next());
+    }
+
+    @Test
+    public void hasNextIsTrueWithTwoElementsAndOneRowTest() {
+        matrix = new Integer[][] { { 0, 1 } };
+        service = new TwoDimensionIteratorService<>(matrix);
+
+        assertTrue(service.hasNext());
+        assertEquals(0, service.next());
+        assertTrue(service.hasNext());
+        assertEquals(1, service.next());
+        assertFalse(service.hasNext());
+        assertNull(service.next());
+    }
+
+    @Test
+    public void hasNextIsTrueWithTwoElementsAndOneColTest() {
+        matrix = new Integer[][] { { 0 }, { 1 } };
+        service = new TwoDimensionIteratorService<>(matrix);
+
+        assertTrue(service.hasNext());
+        assertEquals(0, service.next());
+        assertTrue(service.hasNext());
+        assertEquals(1, service.next());
+        assertFalse(service.hasNext());
+        assertNull(service.next());
+    }
+
+    @Test
+    public void hasNextIsTrueWithTwoElementsAndOneColAndOneEmptyTest() {
+        matrix = new Integer[][] { { }, { 1 } };
+        service = new TwoDimensionIteratorService<>(matrix);
+
+        assertTrue(service.hasNext());
+        assertEquals(1, service.next());
+        assertFalse(service.hasNext());
+        assertNull(service.next());
+    }
+
+    @Test
+    public void hasNextIsTrueWithThreeRowsAndOneColAndTwoElementsTest() {
+        matrix = new Integer[][] { { 2 }, {  }, { 3 } };
+        service = new TwoDimensionIteratorService<>(matrix);
+
+        assertTrue(service.hasNext());
+        assertEquals(2, service.next());
+        assertTrue(service.hasNext());
+        assertEquals(3, service.next());
+        assertFalse(service.hasNext());
+        assertNull(service.next());
     }
 }

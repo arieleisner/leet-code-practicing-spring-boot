7577137083697eb75010fa3fe9e82ff207a6e07f
Ariel Eisner <aeeisnerg@gmail.com>
2026-03-27 12:50:11 +0100

    Use string builder instead of string.

--- src/main/java/com/ariel/eisner/practice/domain/service/ConvertTreeToStringService.java
+++ src/main/java/com/ariel/eisner/practice/domain/service/ConvertTreeToStringService.java
@@ -11,19 +11,17 @@ public class ConvertTreeToStringService implements ConvertTreeToStringUseCase {
         if (tree.length == 0) {
             return "null";
         }
-        return dfsPreOrder(tree, 0, 0).trim();
+        return dfsPreOrder(tree, 0, 0).toString().trim();
     }
 
-    private String dfsPreOrder(int[][] tree, int row, int col) {
+    private StringBuilder dfsPreOrder(int[][] tree, int row, int col) {
         if (row >= tree.length) {
-            return " null";
+            return new StringBuilder(" null");
         }
 
-        String value = " " + tree[row][col];
-
-        value += dfsPreOrder(tree, row + 1, col);
-        value += dfsPreOrder(tree, row + 1, col + 1);
-
-        return value;
+        StringBuilder builder = new StringBuilder(" " + tree[row][col]);
+        builder.append(dfsPreOrder(tree, row + 1, col));
+        builder.append(dfsPreOrder(tree, row + 1, col + 1));
+        return builder;
     }
 }
